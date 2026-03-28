a3b5754a4f663ba76d988dcf58d4acfb21c5d951
Matheus <matheusluan04@gmail.com>
2026-03-27 21:24:47 -0300

    Interação do Usuario

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("qual é seu nome?");
+string nomeDoUsuario = Console.ReadLine()!;
+string mensagemDeAgradecimento = $"obrigado, {nomeDoUsuario}"!;
+
+Console.WriteLine(mensagemDeAgradecimento);
